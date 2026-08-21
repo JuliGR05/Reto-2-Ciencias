5bb97c92c794cee3d39efe7a2411ca141c0785c3
Juliana <julianita.gutierrez2005@gmail.com>
2026-08-21 13:10:42 -0500

    Crear clase Reserva

---
+++ Reserva.java
@@ -0,0 +1,44 @@
+public class Reserva {
+    private int idCliente;
+    private String nombreCliente;
+    private int idReserva;
+    private String fechaReserva;
+    private String fechaVuelo;
+    private int idVuelo; 
+    private String horaVuelo;
+    private String estadoReserva;
+
+    public Reserva(int idCliente, String nombreCliente, int idReserva, String fechaReserva, String fechaVuelo, int idVuelo, String horaVuelo, String estadoReserva){
+        this.idCliente = idCliente;
+        this.nombreCliente = nombreCliente;
+        this.idReserva = idReserva;
+        this.fechaReserva = fechaReserva;
+        this.fechaVuelo = fechaVuelo; 
+        this.idVuelo = idVuelo;
+        this.horaVuelo = horaVuelo;
+        this.estadoReserva = estadoReserva;
+    }
+
+    //Getters
+
+    public int getIdCliente(){
+        return idCliente;
+    }
+
+    public String getNombreCliente(){
+        return nombreCliente;
+    }
+
+    public int getIdReserva(){
+        return idReserva;
+    }
+
+    public String getFechaReserva(){
+        return fechaReserva;
+    }
+
+    public String getFechaVuelo (){
+        return fechaVuelo;
+    }
+
+}
